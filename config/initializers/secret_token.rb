d58ba04a28d1dce519e79e3ab545e4f55a282b6d
Marcos Lopes <marcosmlopes01@gmail.com>
2012-05-24 16:47:03 -0300

    Primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Debateaberto::Application.config.secret_token = '3f4c56ff73ad16926bf76bedfe9a6657dba637fb114ce00bd13c3374e44b2559bd9fc714887cab65fc94ba6f39d2638c220ee51cea80ed80ea920788121d0e26'
