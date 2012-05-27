0151d9b52f887c9955e5978236bff2b95289a208
Marcos Lopes <marcosmlopes01@gmail.com>
2012-05-27 04:04:39 -0300

    Adicionando default_url_options no ambiente de desenvolvimento. Requisito para o devise funcionar.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,6 @@ Debateaberto::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.action_mailer.default_url_options = { :host => 'debateaberto.dev' }
 end
