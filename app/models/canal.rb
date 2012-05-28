dea8865aecb548b29b3439075b4c3174a6e6ba8a
Marcos Lopes <marcosmlopes01@gmail.com>
2012-05-28 11:58:18 -0300

    Adicionando validação ao model canal

--- app/models/canal.rb
+++ app/models/canal.rb
@@ -1,4 +1,7 @@
 class Canal < ActiveRecord::Base
   belongs_to :usuario
   attr_accessible :descricao, :nome, :path
+  validates :path, presence: true, uniqueness: true,
+                   format: { with: /^[a-z\d]+(-[a-z\d]+)*$/i }
+  validates :nome, presence: true
 end
