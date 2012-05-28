9ce0882777ad37238a32acf4369e4a5012ebf3ff
Marcos Lopes <marcosmlopes01@gmail.com>
2012-05-28 18:58:41 -0300

    Adicionando permissão para adicionar mensagem

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,6 +11,9 @@ class Ability
       can :create, Canal do |canal|
         usuario.persisted?
       end
+      can :create, Mensagem do |mensagem|
+        usuario.persisted?
+      end
       can [:update, :destroy], Canal do |canal|
         canal.try(:usuario) == usuario || usuario.papel == 'moderador'
       end
