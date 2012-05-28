40a1ee19ea9ed7dc3c0bf5c6e44a647165a6fe43
Marcos Lopes <marcosmlopes01@gmail.com>
2012-05-28 15:16:48 -0300

    Adicionando helper para a rota omniauth_authorize apontar para o subdominio principal.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def usuario_omniauth_authorize_url(provider, options = {})
+    url = root_url(options).gsub(/\/$/, '')
+    "#{url}#{usuario_omniauth_authorize_path(provider)}"
+  end
 end
