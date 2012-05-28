6aae11610a6de34514e235dca52f04422c17c5e3
Marcos Lopes <marcosmlopes01@gmail.com>
2012-05-28 11:45:52 -0300

    Corrigindo o nome da classe ability

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def current_ability
-    @current_ability ||= AccountAbility.new(current_usuario)
+    @current_ability ||= Ability.new(current_usuario)
   end
 end
