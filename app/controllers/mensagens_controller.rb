29f9254dbcaf592362748380eed4e0a965809904
Marcos Lopes <marcosmlopes01@gmail.com>
2012-05-28 20:11:40 -0300

    Adicionando solicitação ao criar uma mensagem

--- app/controllers/mensagens_controller.rb
+++ app/controllers/mensagens_controller.rb
@@ -1,6 +1,10 @@
 class MensagensController < ApplicationController
   load_and_authorize_resource
 
+  rescue_from CanCan::AccessDenied do |exception|
+    render 'shared/autentica', layout: false
+  end
+
   def create
     @canal = Canal.find_by_path!(request.subdomain)
     @mensagem.canal = @canal
