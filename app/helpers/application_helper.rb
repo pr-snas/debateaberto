8cbd04dda9465f08b6f823ecd6f60dd281e32bff
Marcos Lopes <marcosmlopes01@gmail.com>
2012-05-29 03:08:51 -0300

    Deixando a autenticação via facebook em um único subdominio por hora

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def usuario_omniauth_authorize_url(provider, options = {})
     url = root_url(options).gsub(/\/$/, '')
-    "#{url}#{usuario_omniauth_authorize_path(provider)}"
+    "#{usuario_omniauth_authorize_path(provider)}"
   end
 end
