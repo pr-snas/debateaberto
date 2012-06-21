cdc8edfacf38d5ca3cb3863eedd6429c8afa79b5
Marcos Lopes <marcosmlopes01@gmail.com>
2012-06-21 13:05:17 -0300

    Adicionando marcador de dias nas msgs do chat

--- app/models/mensagem.rb
+++ app/models/mensagem.rb
@@ -2,4 +2,9 @@ class Mensagem < ActiveRecord::Base
   attr_accessible :content
   belongs_to :usuario
   belongs_to :canal
+
+  def primeira_do_dia?
+    first = Mensagem.where('DATE(created_at) = DATE(?)', self.created_at).first
+    return true if first == self
+  end
 end
