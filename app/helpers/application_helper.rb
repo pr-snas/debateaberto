e3e5bdef7133f4c13dfc57ecdc76e9b864d2c856
Marcos Lopes <marcosmlopes01@gmail.com>
2012-07-19 06:32:17 -0300

    Corrigindo link de autenticação do facebook

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   def usuario_omniauth_authorize_url(provider, options = {})
     url = root_url(options).gsub(/\/$/, '')
-    "#{usuario_omniauth_authorize_path(provider)}"
+    path = usuario_omniauth_authorize_path(provider)
+    "#{url}#{path}"
   end
 end
